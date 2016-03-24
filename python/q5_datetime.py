75049f5636276998553b77586ae275a52cf0beb4
Anthony K <anthony.kim@berkeley.edu>
2016-03-23 23:29:01 -0500

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,30 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+a = datetime.strptime(date_start, date_format)
+b = datetime.strptime(date_stop, date_format)
+
+delta = b-a
+print delta.days
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+date_format2 = "%m%d%Y"
+c = datetime.strptime(date_start, date_format2)
+d = datetime.strptime(date_stop, date_format2)
+
+delta = d-c
+print delta.days
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+date_format3 = "%d-%b-%Y"
+e = datetime.strptime(date_start, date_format3)
+f = datetime.strptime(date_stop, date_format3)
+
+delta = f-e
+print delta.days
