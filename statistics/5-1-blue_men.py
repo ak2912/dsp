87888e937dcbecf555073c4a4456e809aaa1525e
Anthony K <anthony.kim@berkeley.edu>
2016-04-09 18:39:35 -0500

    Update and rename 5-1-blue_men.md to 5-1-blue_men.py

---
+++ statistics/5-1-blue_men.py
@@ -0,0 +1,22 @@
+from __future__ import print_function, division
+
+#import thinkstats2
+#import thinkplot
+
+
+import scipy.stats
+
+mu = 178
+sigma = 7.7
+dist = scipy.stats.norm(loc=mu, scale=sigma)
+#type(dist)
+
+#dist.mean(), dist.std()
+
+#dist.cdf(mu-sigma)
+
+low = dist.cdf(177.8)    # 5'10"
+high = dist.cdf(185.4)   # 6'1"
+low, high, high-low
+
+#Approx 34.2% of the U.S. male population is between 5'10" and 6'1"
